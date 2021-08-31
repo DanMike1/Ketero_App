406b81f6c525abc89f680d803419bf5c85a64de3
DanMike1 <it.dan.mekonnen@gmail.com>
2021-08-31 23:30:17 +0300

    repo created and first push

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'screens/login.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: LoginScreen.routeName,
+      routes: {
+        LoginScreen.routeName: (BuildContext context) => LoginScreen(),
+      },
+    );
+  }
+}
