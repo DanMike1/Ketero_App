36a8a299663079c29949dc0f0e633fe089c1e06a
sintayehu-B <61502710+sintayehu-B@users.noreply.github.com>
2021-09-04 03:18:57 -0400

    added global color and listview for the appointment setup

---
+++ lib/model/global.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+Color darkGreyColor = Color(0xFF212128);
+Color greenColor = Color(0xFF28FFBF);
+Color appBarColor = Color(0xFFBCFFB9);
+
+TextStyle AppointmentTitleStyle = TextStyle(
+  fontFamily: "Avenir",
+  fontWeight: FontWeight.bold,
+  fontSize: 28,
+);
+TextStyle AppointmentDarkTitleStyle = TextStyle(
+  fontFamily: "Avenir",
+  fontWeight: FontWeight.bold,
+  fontSize: 30,
+);
